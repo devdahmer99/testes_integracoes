cb43b378b3d2dafbc9674faa897c121455b69b79
eduardo <eduardodahmer99@gmail.com>
2022-01-05 07:26:05 -0300

    Melhorando os processos de criação de tabela e validações no teste de integração com o banco de dados

--- tests/Alura/Leilao/Tests/Integration/Dao/LeilaoDaoTest.php
+++ tests/Alura/Leilao/Tests/Integration/Dao/LeilaoDaoTest.php
@@ -3,9 +3,10 @@
 namespace Alura\Leilao\Tests\Integration\Dao;
 
 
-use Alura\Leilao\Infra\ConnectionCreator;
 use Alura\Leilao\Model\Leilao;
 use Alura\Leilao\Dao\Leilao as LeilaoDao;
+use Exception;
+use PDO as PDOAlias;
 use PHPUnit\Framework\TestCase;
 
 class LeilaoDaoTest extends TestCase
@@ -14,7 +15,7 @@ class LeilaoDaoTest extends TestCase
 
     public static function setUpBeforeClass(): void
     {
-        self::$pdo = new \PDO('sqlite: memory');
+        self::$pdo = new PDOAlias('sqlite: memory');
         self::$pdo->exec('create table leiloes (
                 id INTEGER primary key,
                 descricao  TEXT,
@@ -32,19 +33,17 @@ class LeilaoDaoTest extends TestCase
     
     
     /**
-     * @throws \Exception
+     * @throws Exception
+     * @dataProvider leiloes
      */
-    public function testInserindoEBuscando()
+    public function testBuscaLeiloesNaoFinalizados(array $leiloes)
     {
-        // Montando o Cenário
-      $leilao = new Leilao('Variante 0Km');
-      $leilaoDao = new LeilaoDao(self::$pdo);
-      $leilaoDao->salva($leilao);
-
-        // Agindo e garantindo que o Cenário esta ok
+        $leilaoDao = new LeilaoDao(self::$pdo);
+        foreach ($leiloes as $leilao) {
+            $leilaoDao->salva($leilao);
+        }
       $leiloes = $leilaoDao->recuperarNaoFinalizados();
 
-      // Testando se realmente esta batendo com o desenvolvido
       self::assertCount(1, $leiloes);
       self::assertContainsOnlyInstancesOf(Leilao::class, $leiloes);
       self::assertSame(
@@ -53,8 +52,45 @@ class LeilaoDaoTest extends TestCase
       );
     }
 
+    /**
+     * @param array $leiloes
+     * @dataProvider leiloes
+     */
+    public function testBuscaLeiloesFinalizados(array $leiloes)
+    {
+        $leilaoDao = new LeilaoDao(self::$pdo);
+        foreach ($leiloes as $leilao) {
+            $leilaoDao->salva($leilao);
+        }
+        $leiloes = $leilaoDao->recuperarFinalizados();
+
+        self::assertCount(1, $leiloes);
+        self::assertContainsOnlyInstancesOf(Leilao::class, $leiloes);
+        self::assertSame(
+            'Fia 147 0Km',
+            $leiloes[0]->recuperarDescricao()
+        );
+        self::assertTrue($leiloes[0]->estaFinalizado());
+    }
+
+
     public function tearDown(): void
     {
         self::$pdo->rollBack();
+    }
+
+    public function leiloes(): array
+    {
+        $naoFinalizado = new Leilao('Variante 0Km');
+        $finalizado = new Leilao('Fia 147 0Km');
+        $finalizado->finaliza();
+
+        return [
+            [
+                [
+                    $naoFinalizado, $finalizado
+                ]
+            ]
+        ];
     }
 }
